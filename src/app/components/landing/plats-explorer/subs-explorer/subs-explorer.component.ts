7ecad5b77c8515b34147e031e6d90ffac130f98d
DavMunHer <davidmunyozh@gmail.com>
2025-05-24 23:10:39 +0200

    feat(style): Showing subscriptions from each platform

--- src/app/components/landing/plats-explorer/subs-explorer/subs-explorer.component.ts
+++ src/app/components/landing/plats-explorer/subs-explorer/subs-explorer.component.ts
@@ -1,14 +1,29 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { PlatformName } from '../../../../types/platformName';
+import { PlatsSubsService } from '../../../../services/plats-subs.service';
 
 @Component({
   selector: 'subs-explorer',
   imports: [],
   templateUrl: './subs-explorer.component.html',
-  styleUrl: './subs-explorer.component.css'
+  styleUrl: './subs-explorer.component.css',
 })
 export class SubsExplorerComponent {
   public currentPlatform = input.required<PlatformName>();
+  private platformsService = inject(PlatsSubsService);
+  private platformsInfo = this.platformsService.getStreamingPlatforms();
 
-  
+  protected platformSubscriptions = computed(
+    () =>
+      this.platformsInfo
+        .filter((platformInfo) => platformInfo.name == this.currentPlatform())
+        .map((platform) => platform.subscriptions)[0]
+  );
+
+  // protected subscriptionsCount = computed(() =>
+  //   this.platformSubscriptions()?.reduce((sum, subscription) => {
+  //     return sum + subscription.membershipMonthsDuration.length;
+  //   }, 0)
+  // ); 
+
 }
