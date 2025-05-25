c24a6c1368f6e0b0635a8b2a0c4021a661554356
Oscar Barber Canet <oscarbarber2004@gmail.com>
2025-05-25 12:21:03 +0200

    feat: added banner component

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { LandingNavbarComponent } from "./landing-navbar/landing-navbar.component";
 import { PlatsExplorerComponent } from './plats-explorer/plats-explorer.component';
+import { LandingBannerComponent } from './landing-banner/landing-banner.component';
 
 @Component({
   selector: 'app-landing',
-  imports: [PlatsExplorerComponent, LandingNavbarComponent],
+  imports: [PlatsExplorerComponent, LandingNavbarComponent, LandingBannerComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
-  
+
 }
