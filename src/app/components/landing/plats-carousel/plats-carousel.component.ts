ec17b55294248b35fd6b83ff346d9f76d6c1868c
DavMunHer <davidmunyozh@gmail.com>
2025-06-05 20:17:43 +0200

    feat: Logic template for scrollbar behaviour when clicking arrows

--- src/app/components/landing/plats-carousel/plats-carousel.component.ts
+++ src/app/components/landing/plats-carousel/plats-carousel.component.ts
@@ -4,40 +4,87 @@ import { Component, signal } from '@angular/core';
   selector: 'plats-carousel',
   imports: [],
   templateUrl: './plats-carousel.component.html',
-  styleUrl: './plats-carousel.component.css'
+  styleUrl: './plats-carousel.component.css',
 })
 export class PlatsCarouselComponent {
   cards = signal([
     {
-      subscription: 'crunchiroll',
+      subscription: '1',
       persons: 4,
       price: 25,
-      timestamp: 2
-    }, {
-      subscription: 'crunchiroll',
+      timestamp: 2,
+    },
+    {
+      subscription: '2',
       persons: 4,
       price: 25,
-      timestamp: 2
-    }, {
-      subscription: 'crunchiroll',
+      timestamp: 2,
+    },
+    {
+      subscription: '3',
       persons: 4,
       price: 25,
-      timestamp: 2
-    }, {
-      subscription: 'crunchiroll',
+      timestamp: 2,
+    },
+    {
+      subscription: '4',
       persons: 4,
       price: 25,
-      timestamp: 2
-    }, {
-      subscription: 'crunchiroll',
+      timestamp: 2,
+    },
+    {
+      subscription: '5',
       persons: 4,
       price: 25,
-      timestamp: 2
-    }, {
-      subscription: 'crunchiroll',
+      timestamp: 2,
+    },
+    {
+      subscription: '6',
       persons: 4,
       price: 25,
-      timestamp: 2
-    }
-  ])
+      timestamp: 2,
+    },
+    {
+      subscription: '7',
+      persons: 4,
+      price: 25,
+      timestamp: 2,
+    },
+    {
+      subscription: '8',
+      persons: 4,
+      price: 25,
+      timestamp: 2,
+    },
+    {
+      subscription: '9',
+      persons: 4,
+      price: 25,
+      timestamp: 2,
+    },
+    {
+      subscription: '10',
+      persons: 4,
+      price: 25,
+      timestamp: 2,
+    },
+  ]);
+
+  // FIXME: Refactor this method
+  scrollLeft() {
+    const card = document.querySelector('.card-container');
+    const cardDimension = card?.getBoundingClientRect();
+    const containerWidth = cardDimension?.width;
+    const content = document.querySelector('.content');
+    content!.scrollLeft -= containerWidth! + 20;
+  }
+  
+  // FIXME: Refactor this method
+  scrollRight() {
+    const card = document.querySelector('.card-container');
+    const cardDimension = card?.getBoundingClientRect();
+    const containerWidth = cardDimension?.width;
+    const content = document.querySelector('.content');
+    content!.scrollLeft += containerWidth! + 20;
+  }
 }
