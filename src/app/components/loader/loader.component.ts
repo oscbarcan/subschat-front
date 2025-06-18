6eceb437dc46d44e4ed2083f36a7ce2c7807c1c7
DavMunHer <davidmunyozh@gmail.com>
2025-06-18 20:15:02 +0200

    feat: added logic for being able to change loader size via input

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'loader',
   imports: [],
   templateUrl: './loader.component.html',
-  styleUrl: './loader.component.css'
+  styleUrl: './loader.component.css',
 })
-export class LoaderComponent {
+export class LoaderComponent implements OnInit {
+  private el = inject(ElementRef);
 
+  public loaderSize = input<number>(90);
+
+  ngOnInit(): void {
+    const thisElement = this.el.nativeElement as HTMLElement;
+    thisElement.style.setProperty('--loader-width', `${this.loaderSize()}px`);
+  }
 }
