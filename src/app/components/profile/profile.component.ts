50a4a5b35e968244b63d4f2d435d63d0c935a7d6
Oscar Barber Canet <oscarbarber2004@gmail.com>
2025-07-24 19:57:56 +0200

    feat: new profile and settings section

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -34,4 +34,49 @@ export class ProfileComponent {
       timestamp: 2,
     }
   ]);
+
+  cards2 = signal([
+    {
+      subscription: '1',
+      persons: 4,
+      price: 25,
+      timestamp: 2,
+    },
+    {
+      subscription: '2',
+      persons: 4,
+      price: 25,
+      timestamp: 2,
+    },
+    {
+      subscription: '3',
+      persons: 4,
+      price: 25,
+      timestamp: 2,
+    },
+    {
+      subscription: '4',
+      persons: 4,
+      price: 25,
+      timestamp: 2,
+    },
+    {
+      subscription: '5',
+      persons: 4,
+      price: 25,
+      timestamp: 2,
+    },
+    {
+      subscription: '6',
+      persons: 4,
+      price: 25,
+      timestamp: 2,
+    },
+    {
+      subscription: '7',
+      persons: 4,
+      price: 25,
+      timestamp: 2,
+    }
+  ]);
 }
