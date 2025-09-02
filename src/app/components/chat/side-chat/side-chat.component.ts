66436e2fef9de9fdb250bbbb30b61c0a1a26718f
Oscar Barber Canet <oscarbarber2004@gmail.com>
2025-09-02 16:29:28 +0200

    feat: added timestamp to the side-chat-container

--- src/app/components/chat/side-chat/side-chat.component.ts
+++ src/app/components/chat/side-chat/side-chat.component.ts
@@ -12,73 +12,85 @@ export class SideChatComponent {
       name: 'David',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
     {
       name: 'Pepe',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
     {
       name: 'Programacion',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
     {
       name: 'Crunchyroll Pack',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
     {
       name: 'Crunchyroll Pack',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
     {
       name: 'Crunchyroll Pack',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
     {
       name: 'Crunchyroll Pack',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
     {
       name: 'Crunchyroll Pack',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
     {
       name: 'David',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
     {
       name: 'David',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
     {
       name: 'David',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
     {
       name: 'David',
       last_message: 4,
       img: 'img/default-profile.jpg',
-      lastmsg: 'hola como estas?'
+      lastmsg: 'hola como estas?',
+      time_stamp: '20:48'
     },
   ]);
 }
